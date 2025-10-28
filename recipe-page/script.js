3ddd7078cf9a48e6c1b7ff5ee36228fe55e4a4d7
Siyi Ji <sylviajsyy@gmail.com>
2025-10-27 19:36:13 -0500

    Add a new element to the page

---
+++ recipe-page/script.js
@@ -0,0 +1,6 @@
+// Add a defined new element to ingredient list
+const list = document.getElementById('ingredient-list');
+const newli = document.createElement('li');
+newli.innerHTML = 'Lots of love';
+list.appendChild(newli);
+
