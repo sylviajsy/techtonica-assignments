8940ce7fa62b6b9b586ed3aa6ec2a77d6a1c58bf
Siyi Ji <sylviajsyy@gmail.com>
2025-10-28 23:04:28 -0500

    Adding a check mark as an element to a list

--- recipe-page/script.js
+++ recipe-page/script.js
@@ -4,3 +4,22 @@ const newli = document.createElement('li');
 newli.innerHTML = 'Lots of love';
 list.appendChild(newli);
 
+// Add a check mark as an element to a list
+const firstDiv = document.getElementById('ingredient-div')
+
+// creating checkbox item
+let checkbox = document.createElement('input');
+// Assigning attributes to created checkbox
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+// create label for checkbox
+var label = document.createElement("label");
+label.htmlFor = "id";
+label.appendChild(document.createTextNode("Do you like this recipe?"));
+
+// appending the checkbox and lable the div
+firstDiv.append(checkbox);
+firstDiv.append(label);
